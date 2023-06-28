bf8621766ed4389bc26fcbb1de561eca0beecab4
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-06-28 12:59:44 +0500

    feat: add level switching buttons

--- rs-selectors/src/components/view/header/header.ts
+++ rs-selectors/src/components/view/header/header.ts
@@ -1,14 +1,22 @@
+import './header.scss';
 import { Attributes, ClassesCSS } from '../../../types';
 import BaseComponent from '../../../utils/BaseComponent';
 import { View } from '../View';
 
 const LOGO_TEXT = 'CSS Selectors';
+const PREV_BTN_TEXT = 'Prev';
+const NEXT_BTN_TEXT = 'Next';
+const BADGE_DEFAULT_TEXT = 'Level: 1';
 
 const CSSClasses: ClassesCSS = {
     header: ['header'],
     navBar: ['navbar', 'bg-body-tertiary'],
     navContainer: ['container'],
     navBarLogo: ['navbar-brand', 'fs-2'],
+    navBarRightSide: ['navbar-ritght-side'],
+    lvlSelection: ['level_selection'],
+    lvlBadge: ['badge', 'rounded-pill', 'text-bg-warning'],
+    lvlBtn: ['btn', 'btn-outline-secondary'],
     navBarToggler: ['navbar-toggler'],
     navBarTogglerIcon: ['navbar-toggler-icon'],
 };
@@ -24,6 +32,10 @@ const headerAttributes: Attributes = {
         'data-bs-target': '#offcanvasRight',
         'aria-controls': 'offcanvasRight',
     },
+
+    lvlBtn: {
+        type: 'button',
+    },
 };
 
 export class Header extends View {
@@ -35,15 +47,30 @@ export class Header extends View {
 
     private navBarLogo = new BaseComponent('a', CSSClasses.navBarLogo, this.navContainer);
 
-    private navBarToggler = new BaseComponent('button', CSSClasses.navBarToggler, this.navContainer);
+    private navBarRightSide = new BaseComponent('div', CSSClasses.navBarRightSide, this.navContainer);
+
+    private lvlSelectionContainer = new BaseComponent('div', CSSClasses.lvlSelection, this.navBarRightSide);
+
+    private lvlBadge = new BaseComponent('span', CSSClasses.lvlBadge, this.lvlSelectionContainer);
+
+    private lvlPrevBtn = new BaseComponent('span', CSSClasses.lvlBtn, this.lvlSelectionContainer);
+
+    private lvlNextBtn = new BaseComponent('span', CSSClasses.lvlBtn, this.lvlSelectionContainer);
+
+    private navBarToggler = new BaseComponent('button', CSSClasses.navBarToggler, this.navBarRightSide);
 
     private navBarTogglerIcon = new BaseComponent('span', CSSClasses.navBarTogglerIcon, this.navBarToggler);
 
     constructor() {
         super();
         this.navBarLogo.setTextContent(LOGO_TEXT);
+        this.lvlPrevBtn.setTextContent(PREV_BTN_TEXT);
+        this.lvlNextBtn.setTextContent(NEXT_BTN_TEXT);
+        this.lvlBadge.setTextContent(BADGE_DEFAULT_TEXT);
         this.navBarLogo.setAttributes(headerAttributes.navBarLogo);
         this.navBarToggler.setAttributes(headerAttributes.navBarToggler);
+        this.lvlPrevBtn.setAttributes(headerAttributes.lvlBtn);
+        this.lvlNextBtn.setAttributes(headerAttributes.lvlBtn);
         this.rememberElements('headerComponent', {
             header: this.header,
             navBar: this.navBar,
@@ -51,6 +78,11 @@ export class Header extends View {
             navBarLogo: this.navBarLogo,
             navBarToggler: this.navBarToggler,
             navBarTogglerIcon: this.navBarTogglerIcon,
+            navBarRightSide: this.navBarRightSide,
+            lvlSelectionContainer: this.lvlSelectionContainer,
+            lvlBadge: this.lvlBadge,
+            lvlPrevBtn: this.lvlPrevBtn,
+            lvlNextBtn: this.lvlNextBtn,
         });
     }
 
