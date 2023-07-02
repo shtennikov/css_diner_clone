d7eddfd6e2fbbd2a09f0564fb6039a9b53c5fcbf
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-07-02 14:47:17 +0500

    fix: add an abstract modifier to the View class

--- rs-selectors/src/components/view/View.ts
+++ rs-selectors/src/components/view/View.ts
@@ -1,7 +1,7 @@
 import { AppComponents } from '../../data/AppComponents';
 import { Components } from '../../types';
 
-export class View {
+export abstract class View {
     protected rememberElements(componentName: string, components: Components): void {
         AppComponents[componentName] = components;
     }
