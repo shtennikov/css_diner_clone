00cc4d8868f9d493e5e82acbad0de4b7c8cf9eb3
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-07-04 13:52:23 +0500

    fix: add a prefix for keys in localStorage

--- rs-selectors/src/data/constants.ts
+++ rs-selectors/src/data/constants.ts
@@ -1,9 +1,11 @@
 export const INDENT = '    ';
 export const LESS_THAN = '&lt;';
 export const GREATER_THAN = '&gt;';
-export const STORAGE_LEVEL_KEY = 'level';
-export const STORAGE_PROGRESS_KEY = 'progress';
-export const STORAGE_PASSED_LEVELS_KEY = 'passed';
+
+const PREFIX = 'cssselectorsapp123321_';
+export const STORAGE_LEVEL_KEY = `${PREFIX}level`;
+export const STORAGE_PROGRESS_KEY = `${PREFIX}progress`;
+export const STORAGE_PASSED_LEVELS_KEY = `${PREFIX}passed`;
 
 export const SMALL_CLASS = 'small';
 export const ANIMATED_CLASS = 'animated';
