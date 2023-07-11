86b65ec1502b91f4b7d39817919e7ca9f6f1e7d2
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-07-11 16:40:14 +0500

    refactor: delete unnecessary function

--- src/utils/isSelectorValid.ts
+++
@@ -1,7 +0,0 @@
-export function isSelectorValid(selector: string): boolean {
-    const selectorRegex = /^([a-zA-Z0-9\s.#_~-]+|\[.*?\]|:.*?|>)+$/;
-
-    const isValid = selectorRegex.test(selector);
-
-    return isValid;
-}
